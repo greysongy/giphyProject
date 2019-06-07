9ee9d198843f55aeeb5d32c7461b71f6746cb6d0
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-06-06 18:33:09 -0700

    functionality to populate page with gifs/retrieve a particular emotion using data-attributes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(".apiB").on("click", function () {
-    console.log("This ran");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=cat&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4&limit=5";
+    console.log($(this).attr("data-emotion").toLowerCase());
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=cat&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -9,7 +9,7 @@ $(".apiB").on("click", function () {
 
         .then(function (response) {
             console.log(response);
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 10; i++) {
                 var newImage = $("<img>");
                 newImage.attr("src", response.data[i].images.fixed_width.url);
                 newImage.addClass("border border-black ml-3");
