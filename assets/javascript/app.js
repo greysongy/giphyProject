d912df2202b822395c2ad21844653ac370312ab6
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-06-06 20:18:01 -0700

    Display Modifications/Functionality of Adding New Buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,22 @@
-$(".apiB").on("click", function () {
-    console.log($(this).attr("data-emotion").toLowerCase());
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=cat&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4&limit=10";
+var niceMode = true;
+var ratingMode = false;
+
+$("#submit").on("click", function () {
+    if ($("#emotionInput").val() != "") {
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-light border border-black m-2");
+        newButton.addClass("apiB");
+        console.log($(newButton)[0].classList);
+        newButton.html("<strong>" + $('#emotionInput').val() + "</strong>");
+        newButton.attr("data-emotion", $("#emotionInput").val());
+        $("#buttons").append(newButton);
+    }
+
+})
+
+$(document).on("click", ".apiB", function () {
+    var emotion = $(this).attr("data-emotion").toLowerCase();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "t&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -8,14 +24,70 @@ $(".apiB").on("click", function () {
     })
 
         .then(function (response) {
+            // $("#imgRow").empty();
+            // $("#imgRowLower").empty();
             console.log(response);
             for (var i = 0; i < 10; i++) {
-                var newImage = $("<img>");
-                newImage.attr("src", response.data[i].images.fixed_width.url);
-                newImage.addClass("border border-black ml-3");
-                
-                    $("#imgRow").append(newImage);
-                
+                $("#" + (i + 1)).empty();
             }
+            for (var i = 0; i < 10; i++) {
+                var newImage = $("<img>");
+                newImage.addClass("gif");
+                newImage.attr("data-still", response.data[i].images.fixed_width_still.url);
+                newImage.attr("data-animate", response.data[i].images.fixed_width.url);
+                newImage.attr("data-state", "still");
+                newImage.attr("src", response.data[i].images.fixed_width_still.url);
+                newImage.addClass("border border-black ml-3 mt-3");
+                var rating = $("<h6>Rating");
+                // if (i >= 5) {
+                //     $("#imgRowLower").append(newImage);
+                // }
+                // else {
+                //     $("#imgRow").append(newImage);
+                // }
+                console.log("#" + (i + 1));
+                $("#" + (i + 1)).append(newImage);
+                $("#" + (i + 1)).append("Rating: " + response.data[i].rating);
+            }
+            // for(var i = 0; i < 10; i++) {
+            //     console.log(response.data[i].rating);
+            //     var rating = $("<h6>");
+            //     rating.text("Rating: " + response.data[i].rating);
+            //     if (i >= 5) {
+            //         $("#imgRowLower").append(rating);
+            //     }
+            //     else {
+            //         $("#imgRow").append(rating);
+            //     }
+            // }
         })
 })
+
+$(document).on("click", ".gif", function() {
+    console.log("This ran");
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+// $("#submit").on("click", function () {
+//     if ($("#emotionInput").val() != "") {
+//         var newButton = $("<button>");
+//         newButton.addClass("btn btn-light border border-black m-2");
+//         newButton.addClass("apiB");
+//         newButton.html("<strong>" + $('#emotionInput').val() + "</strong>");
+//         newButton.attr("data-emotion", $("#emotionInput").val());
+//         $("#buttons").append(newButton);
+//     }
+
+// })
+
